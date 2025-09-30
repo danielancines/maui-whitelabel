ec2d26dc9c1a335701b50dcdc85c49cfb2eed3be
Daniel Ancines <daniel.ancines@gmail.com>
2025-09-30 20:58:53 -0300

    Tests for SetTheme on VisualService.

--- src/Maui.WhiteLabelAppTheme.Tests/VisualServiceTests.cs
+++ src/Maui.WhiteLabelAppTheme.Tests/VisualServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
+using Maui.WhiteLabelAppTheme.Resources.Themes;
 using Maui.WhiteLabelAppTheme.Services;
 
 namespace Maui.WhiteLabelAppTheme.Tests;
@@ -21,8 +22,70 @@ public class VisualServiceTests
     {
         var visualService = this._serviceProvider.GetService<IVisualService>();
         
+        if (visualService == null)
+            Assert.Throws<ArgumentNullException>("VisualService", () => { });
+        
         visualService.SetAppTheme(AppTheme.Dark);
-
+        
+        if (Application.Current == null)
+            Assert.Throws<ArgumentNullException>("Application.Current", () => { });
+        
         Assert.Equal(AppTheme.Dark, Application.Current.UserAppTheme);
+    }
+
+    [Fact]
+    public void Set_Light_Theme_Should_Be_Light()
+    {
+        var visualService = this._serviceProvider.GetService<IVisualService>();
+        
+        if (visualService == null)
+            Assert.Throws<ArgumentNullException>("VisualService", () => { });
+        
+        visualService.SetAppTheme(AppTheme.Light);
+        
+        if (Application.Current == null)
+            Assert.Throws<ArgumentNullException>("Application.Current", () => { });
+        
+        Assert.Equal(AppTheme.Light, Application.Current.UserAppTheme);
+    }
+
+    [Fact]
+    public void Set_NTheme_Must_Have_Only_Styles_And_NTheme()
+    {
+        var visualService = this._serviceProvider.GetService<IVisualService>();
+        
+        if (visualService == null)
+            Assert.Throws<ArgumentNullException>("VisualService", () => { });
+        
+        visualService.SetTheme(Theme.NTheme);
+        
+        if (Application.Current == null)
+            Assert.Throws<ArgumentNullException>("Application.Current", () => { });
+
+        var resources = Application.Current.Resources.MergedDictionaries;
+        
+        Assert.Null(resources.FirstOrDefault(r => r.GetType().IsAssignableTo(typeof(BTheme))));
+        Assert.NotNull(resources.FirstOrDefault(r => r.GetType().IsAssignableTo(typeof(NTheme))));
+        Assert.True(resources.Count == 1);
+    }
+    
+    [Fact]
+    public void Set_BTheme_Must_Have_Only_Styles_And_BTheme()
+    {
+        var visualService = this._serviceProvider.GetService<IVisualService>();
+        
+        if (visualService == null)
+            Assert.Throws<ArgumentNullException>("VisualService", () => { });
+        
+        visualService.SetTheme(Theme.BTheme);
+        
+        if (Application.Current == null)
+            Assert.Throws<ArgumentNullException>("Application.Current", () => { });
+
+        var resources = Application.Current.Resources.MergedDictionaries;
+        
+        Assert.Null(resources.FirstOrDefault(r => r.GetType().IsAssignableTo(typeof(NTheme))));
+        Assert.NotNull(resources.FirstOrDefault(r => r.GetType().IsAssignableTo(typeof(BTheme))));
+        Assert.True(resources.Count == 1);
     }
 }
