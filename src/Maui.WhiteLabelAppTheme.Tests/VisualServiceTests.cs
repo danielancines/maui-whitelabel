7b93c10f4597d8dbac53fcdd9fbe423ac37a396a
Daniel Ancines <daniel.ancines@gmail.com>
2025-09-29 21:23:34 -0300

    Unit Tests for AppTheme

---
+++ src/Maui.WhiteLabelAppTheme.Tests/VisualServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+using Maui.WhiteLabelAppTheme.Services;
+
+namespace Maui.WhiteLabelAppTheme.Tests;
+
+public class VisualServiceTests
+{
+    private readonly ServiceProvider _serviceProvider;
+
+    public VisualServiceTests()
+    {
+        ServiceCollection serviceCollection = new();
+        serviceCollection.AddScoped<IVisualService, VisualService>();
+        
+        this._serviceProvider = serviceCollection.BuildServiceProvider();
+        Application.Current = new Application();
+    }
+    
+    [Fact]
+    public void Set_Dark_Theme_Should_Be_Dark()
+    {
+        var visualService = this._serviceProvider.GetService<IVisualService>();
+        
+        visualService.SetAppTheme(AppTheme.Dark);
+
+        Assert.Equal(AppTheme.Dark, Application.Current.UserAppTheme);
+    }
+}
